0a9e04e200fd5ea072d63be79ee1d3ef9b0242c1
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2024-11-25 15:36:03 +0600

    Ddemo App

---
+++ DemoApp/DemoApp/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using DemoApp.DTOs;
+using DemoApp.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        UMSCFAEntities db = new UMSCFAEntities();
+        [HttpGet]
+        public ActionResult Login() { 
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LoginDTO log) {
+            var user = (from u in db.Users
+                        where u.Username.Equals(log.Uname) &&
+                        u.Password.Equals(log.Password)
+                        select u).SingleOrDefault();
+            if (user != null) {
+                Session["user"] = user;
+                return RedirectToAction("List","Student");
+            }
+            return View();
+        }
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ DemoApp/DemoApp/Controllers/StudentController.cs
@@ -0,0 +1,34 @@
+﻿using DemoApp.Auth;
+using DemoApp.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        UMSCFAEntities db = new UMSCFAEntities();
+        public ActionResult List(int pageNo=1,int take=10)
+        {
+            
+            
+            int total = db.Students.Count();
+            int totalPages = total / take;
+            ViewBag.NoOfPage = totalPages;
+
+            var data = db.Students.OrderByDescending(x => x.Id).Skip((pageNo-1)*take).Take(take).ToList();
+            return View(data);
+        }
+        [AdminAccess]
+        public ActionResult Delete(int id) {
+            var data = db.Students.Find(id);
+            db.Students.Remove(data);
+            db.SaveChanges();
+            return RedirectToAction("List");
+        }
+    }
+}

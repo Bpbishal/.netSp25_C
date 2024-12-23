774bf923ed8b379ee511eeb6c85316ba5398a590
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2024-12-23 14:58:50 +0600

    IntroAPi

---
+++ IntroEFCF/IntroEFCF/EF/StudentContext.cs
@@ -0,0 +1,17 @@
+﻿using IntroEFCF.EF.Tables;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace IntroEFCF.EF
+{
+    public class StudentContext : DbContext
+    {
+        //public StudentContext() : base("") { }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Department> Departments { get; set; }
+    }
+}

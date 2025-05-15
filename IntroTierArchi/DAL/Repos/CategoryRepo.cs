e2436ed789097bc95668f22470dc43ec9679e4ed
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2025-05-15 14:06:49 +0600

    Intro tier

---
+++ IntroTierArchi/DAL/Repos/CategoryRepo.cs
@@ -0,0 +1,29 @@
+﻿using DAL.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repos
+{
+    public class CategoryRepo : Repo
+    {
+        public void Create(Category c) { 
+            
+        }
+        public List<Category> Get() { 
+            return new List<Category>();
+        }
+        public Category Get(int id) { 
+            return new Category();
+        }
+        public void Delete(int id) { 
+            
+        }
+
+        public void Update(Category c) { 
+        
+        }
+    }
+}
---
+++ IntroTierArchi/DAL/Repos/ProductRepo.cs
@@ -0,0 +1,33 @@
+﻿using DAL.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repos
+{
+    internal class ProductRepo
+    {
+        public void Create(Product c)
+        {
+
+        }
+        public List<Product> Get()
+        {
+            return new List<Product>();
+        }
+        public Product Get(int id)
+        {
+            return null;
+        }
+        public void Delete(int id)
+        {
+
+        }
+        public void Update(Product c)
+        {
+
+        }
+    }
+}
